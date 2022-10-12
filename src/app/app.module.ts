3490af21b76a1bb587095d0581223f10896dae4f
Ann-Annice <annannice98@gmail.com>
2022-10-12 15:30:41 +0530

    public

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PublicComponent } from './public/public.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PublicComponent
   ],
   imports: [
     BrowserModule,
